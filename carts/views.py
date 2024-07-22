653abc06aef0ded2bb6bc8ac48956b3bcaef052c
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-07-22 07:18:49 +0600

    Fixing Products Warning And Empty Cart Issue

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import get_object_or_404, render, redirect, HttpResponse
+from django.shortcuts import get_object_or_404, render, redirect
 from store.models import Product
 from .models import Cart, CartItem
+from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
@@ -53,6 +55,8 @@ def remove_cart_item(request, product_id):
 
 def cart(request, total=0, quantity=0, cart_item=None):
     try:
+        tax = 0
+        grand_total = 0
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
@@ -60,7 +64,7 @@ def cart(request, total=0, quantity=0, cart_item=None):
             quantity += cart_item.quantity
         tax = (2 * total)/100
         grand_total = total + tax
-    except ObjectNotExist:
+    except ObjectDoesNotExist:
         pass #Just ignore
 
     context = {
