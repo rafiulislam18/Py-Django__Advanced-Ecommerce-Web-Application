b036f33c0d78622fe1347ae680ff6d9f0b40a8a9
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-07-24 05:39:52 +0600

    Registration Preparation, Setting Up URLs & Design

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
 
-# Create your views here.
+
+def register(request):
+    form = RegistrationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/register.html', context)
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    return redirect('login')
