6a97d9ebf1c696d6a867eb371d736352846c4024
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-07-24 19:40:16 +0600

    Register View - correction to a silly mistake

--- accounts/views.py
+++ accounts/views.py
@@ -7,7 +7,7 @@ from .models import Account
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
-        if form.is_valid:
+        if form.is_valid():
             first_name = form.cleaned_data['first_name']
             last_name = form.cleaned_data['last_name']
             phone_number = form.cleaned_data['phone_number']
