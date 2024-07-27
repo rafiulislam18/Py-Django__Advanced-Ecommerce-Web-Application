266d032bb5e67e5bc57d8e42116a9ca8e8e21075
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-07-27 14:22:04 +0600

    Reset Password Validation

--- accounts/views.py
+++ accounts/views.py
@@ -88,7 +88,7 @@ def activate(request, uidb64, token):
         messages.success(request, 'Congratulations! Your account has been activated successfully.')
         return redirect('login')
     else:
-        messages.error(request, 'Invalid activation link.')
+        messages.error(request, 'This link has been expired or is invalid.')
         return redirect('register')
     
 
@@ -125,5 +125,36 @@ def forgotPassword(request):
     return render(request, 'accounts/forgotPassword.html')
 
 
-def resetpassword_validate(request):
-    return HttpResponse('ok')
+def resetpassword_validate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.success(request, 'Please reset your password.')
+        return redirect('resetPassword')
+    else:
+        messages.error(request, 'This link has been expired or is invalid.')
+        return redirect('login')
+    
+
+def resetPassword(request):
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        if password == confirm_password:
+            uid = request.session.get('uid')
+            user = Account.objects.get(pk=uid)
+            user.set_password(password)
+            user.save()
+            messages.success(request, 'Password reset successful.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match.')
+            return redirect('resetPassword')
+    else:
+        return render(request, 'accounts/resetPassword.html')
+
